bf8a46ebb6b52d46f4de43a8eebc752d46bae32e
Eunice Dhivya <divi.seed@gmail.com>
2022-01-01 23:43:20 +0530

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddBox />
+
     </div>
   );
+}
+
+function AddBox() {
+  const [color, setColor] = useState('orange');
+  const styles = { backgroundColor: color };
+  // let test = ['orange', 'teal']
+  const [colorList, setColorList] = useState(['orange'])
+  return (
+    <div>
+      <input
+        style={styles}
+        value={color}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setColor(e.target.value);
+        }}
+        placeholder='Enter a color' />
+      <button onClick={
+        () => {
+          setColorList([...colorList, color]);
+        }
+      }>Add Color</button>
+
+      {
+        colorList.map(function (item) {
+          // console.log(item);
+          return <ColorBox key={item} clr={item} />
+        })
+      }
+    </div>
+
+  )
+}
+
+function ColorBox({ clr }) {
+  const styles = {
+    backgroundColor: clr,
+    height: '30px',
+    width: '250px',
+    marginTop: '20px'
+  }
+  return (
+    <div style={styles}></div>
+  )
+
 }
 
 export default App;
